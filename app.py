0afb00feb685449bca8978a91fbaba47af17c9cf
tomek <tomekzygadlo19@wp.pl>
2025-07-18 10:43:53 +0200

    streamlit init

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center;'>🖼️ Image interpretation</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>🔍 Choose the picture to analyse</h2>", unsafe_allow_html=True)
