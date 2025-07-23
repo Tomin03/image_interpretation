affa2a669ff149dca43e698cf2de0168cfa1acd9
tomek <tomekzygadlo19@wp.pl>
2025-07-23 11:37:11 +0200

    funkcja do generowania opisu z Blip

---
+++ generating_caption.py
@@ -0,0 +1,17 @@
+from transformers import BlipProcessor, BlipForConditionalGeneration
+import torch
+from PIL import Image
+import streamlit as st
+
+@st.cache_resource
+def load_blip():
+    procesor = BlipProcessor().from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+    return procesor, model
+
+def generate_caption(image: Image.Image) -> str:
+    procesor, model = load_blip()
+    inputs = procesor(images=image, return_tensors="pt")
+    outputs = model.generate(**inputs)
+    caption = procesor.decode(outputs[0], skip_special_tokens=True)
+    return caption
